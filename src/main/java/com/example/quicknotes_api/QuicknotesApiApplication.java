f49535db95b672c6c17c04cc0b036d39b12f6e6a
Nathan91187 <natanasnake91@gmail.com>
2026-09-20 13:03:41 +0300

    initial commit

---
+++ src/main/java/com/example/quicknotes_api/QuicknotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quicknotes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicknotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicknotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quicknotes_api/QuicknotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quicknotes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicknotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
