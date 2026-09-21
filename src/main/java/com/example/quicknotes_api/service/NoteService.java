75d44ab9a5f341635c956c1f4253082fdd9378ba
Nathan91187 <natanasnake91@gmail.com>
2026-09-21 20:37:37 +0300

    added crud operations

--- src/main/java/com/example/quicknotes_api/service/NoteService.java
+++ src/main/java/com/example/quicknotes_api/service/NoteService.java
@@ -1,10 +1,14 @@
 package com.example.quicknotes_api.service;
 
+import com.example.quicknotes_api.exceptions.NoteNotFoundException;
 import com.example.quicknotes_api.model.Note;
 import com.example.quicknotes_api.repository.NoteRepository;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class NoteService {
     private final NoteRepository noteRepository;
@@ -13,5 +17,20 @@ public class NoteService {
     }
     public List<Note> getNotes(){
        return noteRepository.findAll();
+    }
+    public Note createNote(Note note){
+        return noteRepository.save(note);
+    }
+    public Note getNoteById(Long id){
+        return noteRepository.findById(id).orElseThrow(()-> new NoteNotFoundException(id));
+    }
+    public void deleteNoteById(Long id){
+         noteRepository.deleteById(id);
+    }
+    public Note putNoteById(Long id, Note note){
+        Note existingNote = noteRepository.findById(id).orElseThrow(()->new NoteNotFoundException(id));
+        existingNote.setTitle(note.getTitle());
+        existingNote.setContent(note.getContent());
+        return noteRepository.save(existingNote);
     }
 }
