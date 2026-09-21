1b1ada4ac9915dd65d07a9f8a9c414ac007bb193
Nathan91187 <natanasnake91@gmail.com>
2026-09-21 20:44:15 +0300

    map note as jpa entity

--- src/main/java/com/example/quicknotes_api/model/Note.java
+++ src/main/java/com/example/quicknotes_api/model/Note.java
@@ -1,13 +1,25 @@
 package com.example.quicknotes_api.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "notes")
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    @Column(nullable = false)
     private String title;
     private String content;
     public Note(Long id, String title, String content){
         this.content = content;
         this.id = id;
         this.title = title;
+    }
+    public Note(){
+
     }
     public Long getId(){
         return id;
@@ -17,5 +29,14 @@ public class Note {
     }
     public String getContent(){
         return content;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
