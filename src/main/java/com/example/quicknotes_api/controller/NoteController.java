d8d16744517664106d27242cdf61d4dc776ed2ac
Nathan91187 <natanasnake91@gmail.com>
2026-09-21 20:42:51 +0300

    included put,delete,post and id get mapping

--- src/main/java/com/example/quicknotes_api/controller/NoteController.java
+++ src/main/java/com/example/quicknotes_api/controller/NoteController.java
@@ -2,10 +2,11 @@ package com.example.quicknotes_api.controller;
 
 import com.example.quicknotes_api.model.Note;
 import com.example.quicknotes_api.service.NoteService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class NoteController {
@@ -14,8 +15,24 @@ public class NoteController {
     public NoteController(NoteService noteService){
         this.noteService = noteService;
     }
+    @PutMapping("/notes/{id}")
+    public void putNoteById(@RequestBody Note note, @PathVariable Long id){
+        noteService.putNoteById(id,note);
+    }
+    @DeleteMapping("/notes/{id}")
+    public void deleteNoteById(@PathVariable Long id){
+        noteService.deleteNoteById(id);
+    }
     @GetMapping("/notes")
     public List<Note> getNotes(){
         return noteService.getNotes();
+    }
+    @GetMapping("/notes/{id}")
+    public Note getNodeById(@PathVariable Long id){
+        return noteService.getNoteById(id);
+    }
+    @PostMapping("/notes")
+    public Note saveNote(@Valid @RequestBody Note note){
+        return noteService.createNote(note);
     }
 }
