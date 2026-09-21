87145e95b084c835708a9ecf5f03a73994de8a6c
Nathan91187 <natanasnake91@gmail.com>
2026-09-21 20:42:14 +0300

    add JpaRepository to NoteRepository

--- src/main/java/com/example/quicknotes_api/repository/NoteRepository.java
+++ src/main/java/com/example/quicknotes_api/repository/NoteRepository.java
@@ -1,9 +1,10 @@
 package com.example.quicknotes_api.repository;
 
 import com.example.quicknotes_api.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface NoteRepository {
-List<Note> findAll();
+public interface NoteRepository extends JpaRepository<Note,Long> {
+
 }
