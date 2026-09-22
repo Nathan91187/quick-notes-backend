6cb7c438e83e308e1f6b3525cd21191cd4db75c3
Nathan91187 <natanasnake91@gmail.com>
2026-09-22 20:57:18 +0300

    refactor: remove validation from Note entity

--- src/main/java/com/example/quicknotes_api/model/Note.java
+++ src/main/java/com/example/quicknotes_api/model/Note.java
@@ -1,7 +1,7 @@
 package com.example.quicknotes_api.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+
 
 @Entity
 @Table(name = "notes")
@@ -9,17 +9,10 @@ public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
     @Column(nullable = false)
     private String title;
     private String content;
-    public Note(Long id, String title, String content){
-        this.content = content;
-        this.id = id;
-        this.title = title;
-    }
     public Note(){
-
     }
     public Long getId(){
         return id;
