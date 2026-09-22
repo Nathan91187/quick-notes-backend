7f7f6a9d9c109931c0f963df979cb43e9322b8c3
Nathan91187 <natanasnake91@gmail.com>
2026-09-22 20:59:35 +0300

    feat: add global exception handling

--- src/main/java/com/example/quicknotes_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/quicknotes_api/exceptions/GlobalExceptionHandler.java
@@ -1,25 +1,79 @@
 package com.example.quicknotes_api.exceptions;
 
+import com.example.quicknotes_api.dto.ErrorResponse;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler{
+    private final Logger logger =
+            LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(NoteNotFoundException.class)
-    public ResponseEntity<String> handleNoteNotFound(NoteNotFoundException ex){
-        return ResponseEntity.status(404).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleNoteNotFound(NoteNotFoundException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(
+                        HttpStatus.NOT_FOUND.value(),
+                        ex.getMessage()
+                        ));
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ResponseEntity<String>> handleValidationException(MethodArgumentNotValidException ex){
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex){
         List<FieldError> errors = ex.getBindingResult()
                 .getFieldErrors();
-        return errors.stream().map((error)-> ResponseEntity.status(400).body(error.getDefaultMessage())).toList();
-
-
+        Map<String, String> messages = errors.stream().collect(
+                Collectors.toMap(
+                        error-> error.getField(),
+                        error-> error.getDefaultMessage()));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Validation failed",
+                        messages));
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleMalformedJson(
+            HttpMessageNotReadableException ex){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Malformed JSON request"
+                ));
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleUnsupportedMethod(
+            HttpRequestMethodNotSupportedException ex){
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new ErrorResponse(
+                        HttpStatus.METHOD_NOT_ALLOWED.value(),
+                        "HTTP method not supported"
+                ));
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedError(
+            Exception exception
+    ){
+        logger.error("Unexpected error occurred", exception);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "Unexpected error occurred"
+                        ));
     }
 }
